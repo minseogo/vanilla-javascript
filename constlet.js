e604a13008e209611da887514f91ab18acb06691
minseogo <liopse7@gmail.com>
2024-05-27 14:37:11 +0900

    바닐라 자바스크립트 기초

---
+++ constlet.js
@@ -0,0 +1,15 @@
+// let study = "프론트앤드";
+// // const myword = "그래 나도 한번" + study + "해보자!";
+// const myword = '그래 나도 한번 ${study} 해보자!';
+// console.log(myword);
+// // console.log("그래 나도 한번 프론트앤드 해보자!");
+
+let _month = [2, 3, 7];
+let _study = ['피그마', '뷰', '리액트'];
+let _name = ['홍길동', '나훈아', '조명섭'];
+
+const words0 = `저는 ${_month[0]}개월간의 ${_study[0]} 공부를 한 ${_name[0]} 입니다.`;
+const words1 = `저는 ${_month[1]}개월간의 ${_study[1]} 공부를 한 ${_name[1]} 입니다.`;
+const words2 = `저는 ${_month[2]}개월간의 ${_study[2]} 공부를 한 ${_name[2]} 입니다.`;
+
+console.log(words0, words1, words2);
